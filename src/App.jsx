3d1fd94a093c424d1b3ec7a90a5af54273057604
Demo Alpha <demoalpha24@gmail.com>
2024-11-27 18:12:31 +0530

    ImplementlocalStorage in the form

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import Card from "./components/Card";
 import { cardContext } from "./context/context";
+import js from "@eslint/js";
 
 function App() {
-  const [steps, setsteps] = useState(1); // use to highlight the steps in the sidebar
+  const [steps, setsteps] = useState(() => {  // use to highlight the steps in the sidebar
+    // Initialize from localStorage or default to 1
+    const savedSteps = localStorage.getItem("formSteps");
+    return savedSteps !== null ? JSON.parse(savedSteps) : 1;
+  });
+  
   const [toggle, settoggle] = useState("left"); //used for the toggle button
   const [totalBill, settotalBill] = useState([]); //It's the array of object which consist of summary of Selected Plan
   const [finalAddons, setfinalAddons] = useState([]); //It's the array of object which consist of summary of Addons
-  const [displayThankyou, setdisplayThankyou] = useState(false);
+  const [displayThankyou, setdisplayThankyou] = useState(false); //use to display the Thankyou page of the form
+
+
+  useEffect(() => {
+    localStorage.setItem("formSteps", JSON.stringify(steps));
+  }, [steps]);
+
   return (
     <>
       <cardContext.Provider
